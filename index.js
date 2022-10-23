6d3dd9c80de7f01421e40a3b1e81c5b84669e572
yankiozdogan <yankiozdogan@gmail.com>
2022-10-23 12:35:57 +0300

    showTime double digit update

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ let promptName = document.getElementById("myName")
 promptName.textContent = prompt("İsminizi girin")
 function showTime() {
     let now = new Date();
-    document.getElementById('myClock').innerHTML = now.getHours()+":"+now.getMinutes()+":"+
-    now.getSeconds();
+    let hoursnow = (now.getHours().toString().length==2) ? now.getHours() : "0"+now.getHours();
+    let minutesnow = (now.getMinutes().toString().length==2) ? now.getMinutes() : "0"+now.getMinutes();
+    let secondsnow = (now.getSeconds().toString().length==2) ? now.getSeconds() : "0"+now.getSeconds();
+
+    document.getElementById('myClock').innerHTML = hoursnow + ":" + minutesnow + ":" +
+        secondsnow;
     setTimeout(showTime, 10);
 }
 showTime()
